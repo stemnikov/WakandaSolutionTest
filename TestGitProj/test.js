0f2d218a6a8be75fbc50e1aaaff4764d32d6bc91
stemnikov <Sergiy.Temnikov@4d.com>
2012-03-09 17:44:06 +0100

    Update TestGitProj/test.js

--- TestGitProj/test.js
+++ TestGitProj/test.js
@@ -1,1 +1,23 @@
-﻿/*** @author admin*/function myTest ( inN ){	var		p = inN * 19;	return p;}var		i = 0;i++;i++;i = myTest ( 33 );i = myTest ( 1919 );// My modif from the second local repository i = myTest ( 8080 );i = myTest ( 5454 );i = myTest ( 20230212 );
+﻿/**
+
+* @author admin
+
+*/
+
+function myTest ( inN )
+{
+	var		p = inN * 19;
+
+	return p;
+}
+
+var		i = 0;
+i++;
+i++;
+i = myTest ( 33 );
+i = myTest ( 1919 );
+// My modif from the second local repository 
+i = myTest ( 8080 );
+i = myTest ( 5454 );
+i = myTest ( 20230212 );
+i = myTest ( 707070 ); // Modified directly via github html interface
