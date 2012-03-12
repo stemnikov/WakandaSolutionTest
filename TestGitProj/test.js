b8c7d5e17d3de6949b8af0d2322195dd25813f06
unknown <STemnikov@i7-64b-RD-12.private.4d.fr>
2012-03-12 17:50:50 +0100

    hardcoded push comment

--- TestGitProj/test.js
+++ TestGitProj/test.js
@@ -19,6 +19,6 @@ i = myTest ( 1919 );
 // My modif from the second local repository 
 i = myTest ( 8080 );
 i = myTest ( 5454 );
-i = myTest ( 20230212 );
+i = myTest ( 20230212 ); // Dynamic git root test
 i = myTest ( 707070 ); // Modified directly via github html interface
 // Yet    another    line    from    local    clone   
