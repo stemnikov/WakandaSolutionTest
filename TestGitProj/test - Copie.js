46cc05f1c37a41578124254ab367084054fc2a0a
unknown <STemnikov@OPT990-RD-ST.private.4d.fr>
2012-03-23 15:22:40 +0100

    hardcoded push comment

---
+++ TestGitProj/test - Copie.js
@@ -0,0 +1,43 @@
+﻿/**
+
+* @author admin
+
+*/
+
+// This is a newline for my demo
+// This line has been added directly on github.com
+
+function myTest ( inN )  
+{
+	var		p = inN * 19;     
+	var		oo = 456;
+	var		t1 = 1;
+	var		t2 = 2;
+	var		t3 = 3;  
+
+	return pppp;
+}
+
+var		i = 0;
+i = 1237; 
+var		r3 = khjkhjkh2;
+i++;
+i++;
+var r = 44444444;
+var r5 = 55555555;
+
+// asjdasjdjsagdsadsdsf
+// mofid
+
+var sd = 456;
+sd++;
+
+i = myTest ( 33 );
+i = myTest ( 1919 );
+// My modif from the second local repository 
+// 222 My modif from the second local repository 
+i = myTest ( 8080 );
+i = myTest ( 5454 );
+i = myTest ( 20230212 ); // Dynamic git root test
+i = myTest ( 707070 ); // Modified directly via github html interface
+// Yet    another    line    from    local    clone   
